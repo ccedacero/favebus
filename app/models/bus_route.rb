830167bf753e428fe1d8cd2f607b9ade231fa8a8
ccedacero <cristian.cedacero@gmail.com>
2020-06-17 17:50:06 -0400

    fix user profile page

--- app/models/bus_route.rb
+++ app/models/bus_route.rb
@@ -1,7 +1,6 @@
 class BusRoute < ApplicationRecord
     has_many :rider_routes , dependent: :destroy 
     has_many :riders, through: :rider_routes
-    has_many :route_datums
 
     def format_str(str)
         string = str.split("T")
