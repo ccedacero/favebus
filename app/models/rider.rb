2fa5fa638bc30af02964d310ffd32e6e7de98082
ccedacero <cristian.cedacero@gmail.com>
2020-06-18 13:29:59 -0400

    presented product

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,7 +1,8 @@
 class Rider < ApplicationRecord
     has_many :rider_routes , dependent: :destroy 
-    has_many :bus_routes, through: :rider_routes
+    has_many :bus_routes, through: :rider_routes ,dependent: :destroy
     has_secure_password
     validates :name, presence: true 
        
 end
+# //added the dependent for bus_routes here 
